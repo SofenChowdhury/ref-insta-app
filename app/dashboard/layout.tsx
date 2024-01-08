4ddcaf1b2b3e4f7b913b298beaf672fea15b9023
Sofen <md.rabby.mahmud@gmail.com>
2024-01-08 14:56:01 +0600

    Add tailwind css class for layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,8 +6,8 @@ export default function DashboardLayout({
   children: React.ReactNode
 }) {
   return (
-    <div>
-        <div>
+    <div className="flex h-screen relative flex-col md:flex-row md:overflow-hidden">
+        <div className="w-20 flex-none lg:w-64 md:border-r">
             <SideNav />
         </div>
     </div>
